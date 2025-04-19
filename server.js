6a23c10aa732b2e118f6f17d4af766144fa2bed8
Ipingenieria <ipingenieriayautomatismo@gmail.com>
2025-04-18 19:46:32 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
-// Backend de ejemplo
-console.log('Servidor backend iniciado');
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('¡Sistema IP INGENIERIA funcionando correctamente desde Render!');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
+});
